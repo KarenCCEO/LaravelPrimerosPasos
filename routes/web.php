792317b570f65b8eedd60c3d60c6aa7b33c0791c
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-11 15:01:42 -0500

    Revert "Menu deOpciones segun la base de datos"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Dashboard\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('post', PostController::class);
-//Route::get('post',[PostController::class,'index']);
-//Route::get('post/{post}',[PostController::class,'show']);
-//Route::get('post/create',[PostController::class,'create']);
-//Route::get('post/{post}/edit',[PostController::class,'edit']);
-
-//Route::post('post',[PostController::class,'store']);
-//Route::put('post/{post}',[PostController::class,'update']);
-//Route::delete('post/{post}',[PostController::class,'delete']);
