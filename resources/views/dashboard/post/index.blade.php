6d0a3bc5ae47214b5c4432c19da3e17b9a007388
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-14 21:30:04 -0500

    Subir imagenes

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -1,8 +1,11 @@
 @extends('dashboard.layout')
+
 @section('content')
-    <a href="{{ route("post.create" )}}">Crear</a>
-    <table class="table">
-        <thead class="table-dark">
+
+    <a class="btn btn-success my-3" href="{{ route("post.create") }}">Crear</a>
+
+    <table class="table mb-3">
+        <thead>
         <tr>
             <th>
                 Titulo
@@ -18,35 +21,37 @@
             </th>
         </tr>
         </thead>
+
         <tbody>
-            @foreach($posts as $p)
-                <tr>
-                    <td>
-                        {{$p->title}}
-                    </td>
-                    <td>
-                        categoria
-                    </td>
-                    <td>
-                        {{$p->posted}}
-                    </td>
-                    <td>
-                        <a href="{{ route("post.edit", $p )}}">Editar</a>
-                        <a href="{{ route("post.show", $p )}}">Ver</a>
-                        <form action="{{ route("post.destroy", $p )}}" method="post">
-                            @method("DELETE")
-                            @csrf
-                            <button type="submit">Eliminar</button>
-                        </form>
+        @foreach ($posts as $p)
+            <tr>
+                <td>
+                    {{ $p->title }}
+                </td>
+                <td>
+                    {{ $p->category->title }}
+                </td>
+                <td>
+                    {{ $p->posted }}
+                </td>
+                <td>
+                    <a class="mt-2 btn btn-primary" href="{{ route("post.edit", $p) }}">Editar</a>
+                    <a class="mt-2 btn btn-primary" href="{{ route("post.show", $p) }}">Ver</a>
 
-                    </td>
-                </tr>
-            @endforeach
+                    <form  action="{{ route("post.destroy", $p) }}" method="post">
+                        @method("DELETE")
+                        @csrf
+                        <button class="mt-2 btn btn-danger" type="submit">Eliminar</button>
+                    </form>
 
+
+                </td>
+            </tr>
+        @endforeach
         </tbody>
 
     </table>
-    {{$posts->links()}}
+
+    {{ $posts->links() }}
 
 @endsection
-
