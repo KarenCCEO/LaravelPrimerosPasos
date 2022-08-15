23ccd15609f17020c4333594dcf3e51f9b24870c
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-14 20:08:01 -0500

    Show detalles

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -84,7 +84,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        echo "show";
+        return view("dashboard.post.show",compact('post'));
     }
 
     /**
